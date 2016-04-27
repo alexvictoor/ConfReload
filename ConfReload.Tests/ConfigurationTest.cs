0294c77b243c7beda059ca4ec4c562953b657f04
alexvictoor <alexvictoor@gmail.com>
2016-04-27 22:13:00 +0200

    Initial version

---
+++ ConfReload.Tests/ConfigurationTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NFluent;
+using NUnit.Framework;
+
+namespace ConfReload.Tests
+{
+    public class ConfigurationTest
+    {
+        private Configuration _configuration;
+
+        [Test]
+        public void Should_read_configuration()
+        {
+            // given
+            _configuration = Configuration.Create("resources/example.config");
+            // when
+            var url = _configuration.GetValue("url");
+            // then
+            Check.That(url).IsEqualTo("http://localhost");
+        }
+
+        [Test]
+        public void Should_get_new_configuration_when_file_is_updated()
+        {
+            // given
+            var path = "resources/update.config";
+            _configuration = Configuration.Create(path);
+            // when
+            WriteFileContent(path, "<conf><url>http://new</url></conf>");
+            // then
+            var url = _configuration.GetValue("url");
+            Check.That(url).IsEqualTo("http://new");
+        }
+
+        [Test]
+        public void Should_keep_old_configuration_when_new_configuration_invalid()
+        {
+            // given
+            var path = "resources/update2bad.config";
+            _configuration = Configuration.Create(path);
+            // when
+            WriteFileContent(path, "<conf><url>http://new</url></a></conf>");
+            // then
+            var url = _configuration.GetValue("url");
+            Check.That(url).IsEqualTo("http://localhost");
+        }
+
+        public void WriteFileContent(string path, string content)
+        {
+            using (var stream = new FileStream(path, FileMode.Truncate, FileAccess.Write))
+            {
+                var bytes = Encoding.UTF8.GetBytes(content);
+                stream.Write(bytes, 0, bytes.Length);
+            }
+            Thread.Sleep(300);
+        }
+
+        [TearDown]
+        public void CleanUpConfiguration()
+        {
+            if (_configuration != null)
+            {
+                _configuration.Dispose();
+            }
+        }
+    }
+}
---
+++ ConfReload/Configuration.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConfReload
+{
+    public class Configuration : IDisposable
+    {
+        private volatile XmlDocument _document;
+        private volatile bool _outOfDate = true;
+        private readonly FileSystemWatcher _watcher;
+        private readonly Object _syncRoot = new object();
+        private readonly string _filePath;
+
+        public static Configuration Create(string filePath)
+        {
+            var fileInfo = new FileInfo(filePath);
+            if (!fileInfo.Exists)
+            {
+                throw new Exception("Configuration not found for path " + fileInfo.FullName);
+            }
+            return new Configuration(filePath);
+        }
+
+        private Configuration(string filePath)
+        {
+            _filePath = filePath;
+            RefreshConfiguration();
+            var fileInfo = new FileInfo(filePath);
+            string confFileName = fileInfo.Name;
+            string directoryPath = fileInfo.Directory.FullName;
+            _watcher = new FileSystemWatcher(directoryPath);
+            _watcher.Changed += (sender, eventArgs) =>
+            {
+                if (eventArgs.Name == confFileName)
+                {
+                    RefreshConfiguration();
+                }
+            };
+            _watcher.EnableRaisingEvents = true;
+        }
+
+        public void Dispose()
+        {
+            if (_watcher != null)
+            {
+                _watcher.Dispose();
+            }
+        }
+
+
+        public string GetValue(string key)
+        {
+            if (_outOfDate)
+            {
+                RefreshConfiguration();
+            }
+            var node = _document.SelectSingleNode("*/" + key);
+            if (node == null)
+            {
+                return "unknown";
+            }
+            return node.InnerText;
+        }
+
+        private void RefreshConfiguration()
+        {
+            lock (_syncRoot)
+            {
+                _outOfDate = true;
+                var doc = new XmlDocument();
+                try
+                {
+                    using (var stream = new FileStream(_filePath, FileMode.Open, FileAccess.Read))
+                    {
+                        doc.Load(stream);
+                    }
+                    _document = doc;
+                    _outOfDate = false;
+                }
+                catch (Exception ex)
+                {
+                    // TODO replace with a common logging call
+                    Console.Error.WriteLine("Error while parsing configuration at " + _filePath);
+                }
+            }
+        }
+    }
+}
